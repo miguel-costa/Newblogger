3c1fa3f4d83f87e70dfe14dc5f2cbf27ae74afcf
Miguel Ângelo Costa <miguelcosta@MacBook-Pro-de-Miguel-3.local>
2014-09-18 16:08:04 +0100

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+    def show
+        @tag = Tag.find(params[:id])
+    end
+    def index
+        @tags = Tag.all
+    end
+    def new
+        @tag = Tag.new
+    end
+
+    def destroy
+       @tag = Tag.find(params[:id]) 
+       @tag.destroy
+
+       flash.notice = "Tag '#{@tag.name}' deleted!"
+
+       redirect_to tags_path
+    end
+end
